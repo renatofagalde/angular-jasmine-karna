28e58d286e0d82b1b576493d9fc97172373eba72
Renato Fagalde <renato@likwi.com.br>
2021-10-13 10:34:21 -0300

    #2 definição das propriedades

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
   selector: 'app-photo-frame',
@@ -7,4 +7,20 @@ import {Component} from "@angular/core";
 })
 
 export class PhotoFrameComponent {
+
+  @Input()
+  public description: string;
+
+  @Input()
+  public src: string;
+
+  @Input()
+  public likes = 0;
+
+  @Output()
+  public liked: EventEmitter<void> = new EventEmitter();
+
+  public like() {
+    this.liked.emit();
+  }
 }
