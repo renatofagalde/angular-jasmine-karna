b497186ba3151419e9ca506a5583775a1ba4c1b9
Renato Fagalde <renato@likwi.com.br>
2021-10-23 11:34:58 -0300

    #2 DOM Test, keyboard and click

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,7 +1,7 @@
-import {LikeWidgetComponent} from "./like-widget.component";
-import {ComponentFixture, TestBed} from "@angular/core/testing";
-import {UniqueIdService} from "../../services/unique-id/unique-id.service";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {LikeWidgetComponent} from './like-widget.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {UniqueIdService} from '../../services/unique-id/unique-id.service';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 describe(LikeWidgetComponent.name, () => {
 
@@ -16,20 +16,20 @@ describe(LikeWidgetComponent.name, () => {
       // imports: [LikeWidwetModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LikeWidgetComponent)
+    fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
     // fixture.detectChanges(); // nao é boa pratica
   });
 
   it(`Should create component`, () => {
     const component = fixture.componentInstance;
-    expect(component).toBeTruthy()
+    expect(component).toBeTruthy();
   });
 
   it(`Should auto-generate ID during ngOnInit when (@Input id) is not assigned`, () => {
     // const component = fixture.componentInstance;
     fixture.detectChanges(); //chamar o ngonit na mao
-    expect(component.id).toBeTruthy()
+    expect(component.id).toBeTruthy();
   });
 
   it(`Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned`, () => {
@@ -57,4 +57,57 @@ describe(LikeWidgetComponent.name, () => {
       expect(component.liked.emit).toHaveBeenCalled();
     });
 
-})
+  // it(`#${LikeWidgetComponent.prototype.likeInWidget.name}  should trigger (@Output liked) when called - soft version`,
+  //   () => {
+  //     spyOn(component.liked, 'emit');
+  //     fixture.detectChanges();
+  //     component.likeInWidget();
+  //     expect(component.liked.emit).toHaveBeenCalled();
+  //   });
+  //
+
+  it(`(D) Should display number of likes when clicked`, done => {
+    fixture.detectChanges(); //start ngOnInit
+    component.liked.subscribe(() => {
+      component.likes++;
+      //
+      // //após o clique, deve ser atualizado para notificar o DOM
+      fixture.detectChanges();
+      //
+      const counterEL: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEL.textContent.trim()).toBe('1');
+      done();
+    });
+
+    const likeWidgetConteinerEL: HTMLElement = fixture.nativeElement.querySelector('.like-widget-container');
+    likeWidgetConteinerEL.click();
+
+    //poderia ser direto, porém este é o teste funcional
+    //este teste é para ser feito simulando o clique.
+    //component.likes();
+  });
+
+  it(`(D) Should display number of likes when ENTER key is pressed`, done => {
+    fixture.detectChanges(); //start ngOnInit
+
+    component.liked.subscribe(() => {
+      component.likes++;
+      // //após o clique, deve ser atualizado para notificar o DOM
+      fixture.detectChanges();
+      const counterEL: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEL.textContent.trim()).toBe('1');
+      done();
+    });
+
+    const likeWidgetConteinerEL: HTMLElement = fixture.nativeElement.querySelector('.like-widget-container');
+    //keyup -> igual o evento que está no html.
+    const event = new KeyboardEvent('keyup',{key: 'Enter'})
+    likeWidgetConteinerEL.dispatchEvent(event);
+
+    //exemplo
+    //const event = new KeyboardEvent('keyup',{key: 'ArrowIp'})
+
+  });
+
+
+});
